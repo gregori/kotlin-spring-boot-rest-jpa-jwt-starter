72642bf74916099722a937930d8a996685cb340b
Oliver Sahner <osahner@medianet.de>
2019-06-24 16:28:41 +0200

    chore: cleanup, dep updates

--- src/main/kotlin/osahner/config/AdditionalWebConfig.kt
+++ src/main/kotlin/osahner/config/AdditionalWebConfig.kt
@@ -6,9 +6,6 @@ import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-
 
 
 @Configuration
@@ -35,11 +32,5 @@ class AdditionalWebConfig : WebMvcConfigurer {
     val source = UrlBasedCorsConfigurationSource()
     source.registerCorsConfiguration("/**", configuration)
     return source
-  }
-
-  // Avoid test graphql startup error
-  @Bean
-  fun servletWebServerFactory(): ServletWebServerFactory {
-    return TomcatServletWebServerFactory()
   }
 }
